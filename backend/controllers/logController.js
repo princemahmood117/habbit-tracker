2a0aa189aa926cf26027607efad55f933eed0bd3
iftekhar <princemahmood117@gmail.com>
2026-05-23 00:49:42 +0600

    mark complete

--- backend/controllers/logController.js
+++ backend/controllers/logController.js
@@ -1,11 +1,42 @@
+import Habit from "../models/Habit.js";
+import HabitLog from "../models/HabitLog.js";
+import { todayKey } from "../utils/dateHelpers.js";
 
 
 export const markComplete = async (req, res) => {
 
     try {
+        const {habitId, date} = req.body;
+        const completedDate = date || todayKey();
+        const habit = await Habit.findOne({
+            _id : habitId,
+            userId : req.user._id
+        })
+
+        if(!habit) {
+            res.status(404).json({message: `Habit not found!`})
+        }
+        const log = await HabitLog.findOneAndUpdate({
+            userId: req.user._id,
+            habitId,
+            completedDate
+        },
+        {
+            $setOnInsert : {
+                userId: req.user._id,
+                habitId,
+                completedDate
+            }
+        },
+        {
+            upsert: true,
+            new: true
+        }
+    );
+    res.status(201).json(log)
         
     } catch (error) {
-        
+        res.status(500).json({message: `error from markComplete: ${error}`})
     }
 } 
 
@@ -17,7 +48,7 @@ export const markComplete = async (req, res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message: `error from markComplete: ${error}`})
     }
 } 
 
@@ -28,7 +59,7 @@ export const markComplete = async (req, res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message: `error from markComplete: ${error}`})
     }
 } 
 
@@ -39,7 +70,7 @@ export const markComplete = async (req, res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message: `error from markComplete: ${error}`})
     }
 } 
 
@@ -50,7 +81,7 @@ export const markComplete = async (req, res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message: `error from markComplete: ${error}`})
     }
 } 
 
@@ -61,7 +92,7 @@ export const markComplete = async (req, res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message: `error from markComplete: ${error}`})
     }
 } 
 
@@ -72,7 +103,7 @@ export const markComplete = async (req, res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message: `error from markComplete: ${error}`})
     }
 } 
 
@@ -83,7 +114,7 @@ export const markComplete = async (req, res) => {
     try {
         
     } catch (error) {
-        
+        res.status(500).json({message: `error from markComplete: ${error}`})
     }
 } 
 
